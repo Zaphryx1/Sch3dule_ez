101f70ad9f2fdddec19b7bc723d63a623b859070
Zaphryx1 <michaele.gaytan@gmail.com>
2022-03-09 03:47:11 -0600

    created .js and made on click command

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,15 @@
 // user inputs info
-
+console.log("Hi");
 // save the info to the local storage
+
+var input = "textarea";
+
+
+//save button response
+$(".saveBtn").click(function() {
+
+
+
+});
 
 //keep information updated in specific hour
