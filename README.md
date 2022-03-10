a4d73e5521aae8c497e5b6e1752701b804bfb39a
Zaphryx1 <michaele.gaytan@gmail.com>
2022-03-10 12:24:54 -0600

    adjusted files

---
+++ README.md
@@ -0,0 +1,2 @@
+# Work Day Scheduler Starter Code
+# Sch3dule_ez
---
+++ script.js
@@ -0,0 +1,15 @@
+// user inputs info
+
+// save the info to the local storage
+
+
+
+//save button response
+$(".saveBtn").click(function() {
+
+
+
+
+});
+
+//keep information updated in specific hour
