375d0ba14cc3027380cc343f8ed54876cbdb247c
Zaphryx1 <michaele.gaytan@gmail.com>
2022-03-12 11:28:18 -0600

    able to save items and load()

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
-// user inputs info
+//load what local storage when page loads up 
+function load() {
 
-// save the info to the local storage
+    localStorage.getItem($(".description").val());
+
+    $("#0 .description").val(localStorage.getItem("0"));
+
+    for (var i = 0; i < 9; i++) {
+        $("#" + [i] + " .description").val(localStorage.getItem([i]));
+    }
+}
+
 
 
 
 //save button response
 $(".saveBtn").click(function() {
-
-
-
-
+    // console.log($(this).siblings(".description").val());
+    // save the info to the local storage
+    localStorage.setItem($(this).parent().attr("id"), $(this).siblings(".description").val());
 });
 
 //keep information updated in specific hour
+
+//past hour 
+
+//future hour
+
+//
+
+load();
